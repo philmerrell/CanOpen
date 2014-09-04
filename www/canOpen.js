12a96b978e0cbf0ed4818934419a83dd24fdecb4
Robert Anderson <noisysocks@users.noreply.github.com>
2014-09-04 13:57:43 +1000

    update JS interface to use CommonJS convention

--- www/canOpen.js
+++ www/canOpen.js
@@ -1,19 +1,14 @@
-(function(window) {
-	var cordovaRef = window.Phonegap || window.Cordova || window.cordova;
-
-	window.CanOpen = function(app, callback) {
-		cordovaRef.exec(
-			// Success callback
-			callback,
-			// Failure callback
-			function(err) { console.log('Missing app scheme.');},
-			// Native Class Name 
-			"CanOpen",
-			// Name of method in native class.
-			"appCanOpen",
-			// array of args to pass to method.
-			[app]
-		);
-	};
-
-})(window);
+module.exports = function CanOpen(app, callback) {
+	cordovaRef.exec(
+		// Success callback
+		callback,
+		// Failure callback
+		function(err) { console.log('Missing app scheme.');},
+		// Native Class Name 
+		"CanOpen",
+		// Name of method in native class.
+		"appCanOpen",
+		// array of args to pass to method.
+		[app]
+	);
+};
